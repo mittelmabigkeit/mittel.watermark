2770e807e31473e7400c57b659d8486b4800618d
mittelmabigkeit <goodnazi1488@gmail.com>
2026-01-20 14:56:28 +0300

    Create index.php

---
+++ .last_version/lang/ru/install/index.php
@@ -0,0 +1,4 @@
+<?
+$MESS['IMYIE.MORE_WATERMARK.MODULE_INSTALL_NAME'] = 'Íàëîæåíèå âîäÿíîãî çíàêà.';
+$MESS['IMYIE.MORE_WATERMARK.INSTALL_DESCRIPTION'] = 'Ìîäóëü ïîçâîëÿåò äîáàâëÿòü âîäÿíîé çíàê äëÿ èçîáðàæåíèé.';
+$MESS['IMYIE.MORE_WATERMARK.INSTALL_COPMPANY_NAME'] = 'Åðîôååâ Àðòåì';
